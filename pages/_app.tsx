ae805a9627ea5dd8001b28e066c8b75262ba7ec4
Mirdad1224 <ali.mirdad@yahoo.com>
2022-12-29 17:20:28 +0330

    edit _app.tsx file and add meta tags and google analitics script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,58 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import "locomotive-scroll/dist/locomotive-scroll.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { LazyMotion, domAnimation } from "framer-motion";
+
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import Script from "next/script";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <link
+          rel="shortcut icon"
+          href="/rounded-avatar.png"
+          type="image/x-icon"
+        />
+
+        <title>{`Ali Mirdad Portfolio`}</title>
+
+        <meta name="title" content="Ali Mirdad Portfolio" />
+        <meta name="description" content="Ali Mirdad personal website" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Ali Mirdad Portfolio" />
+        <meta property="og:description" content="Ali Mirdad personal website" />
+        <meta property="og:image" content="/avatar.jpg" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:title" content="Ali Mirdad Portfolio" />
+        <meta
+          property="twitter:description"
+          content="Ali Mirdad personal website"
+        />
+        <meta property="twitter:image" content="/avatar.jpg" />
+      </Head>
+      <LazyMotion features={domAnimation}>
+        <Component {...pageProps} />
+      </LazyMotion>
+      <Script
+        strategy="lazyOnload"
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      ></Script>
+      <Script id="google-analytics" strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+        `}
+      </Script>
+    </>
+  );
 }
+
+export default MyApp;
