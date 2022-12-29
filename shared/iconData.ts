09cc8c2939c1a2cd1b81f7ed73daa1fc43a923ec
Mirdad1224 <ali.mirdad@yahoo.com>
2022-12-29 17:18:48 +0330

    create iconData file

---
+++ shared/iconData.ts
@@ -0,0 +1,41 @@
+export const canvasImages = [
+  "html",
+  "javascript",
+  "css",
+  "react",
+  "typescript",
+  "nodejs",
+  "tailwindcss",
+  "sass",
+  "next",
+  "vscode",
+  "git",
+  "vite",
+  "webpack",
+].map(
+  (icon) =>
+    `https://images.weserv.nl/?url=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FPKief%2Fvscode-material-icon-theme%40main%2Ficons%2F${icon}.svg&w=50&h=50`
+);
+
+export const socialLinks = [
+  {
+    title: "Github",
+    icon: "/github.png",
+    link: "https://github.com/mirdad1224",
+  },
+  {
+    title: "Instagram",
+    icon: "/instagram.png",
+    link: "https://www.instagram.com/ali.mirdad",
+  },
+  {
+    title: "Discord",
+    icon: "/discord.png",
+    link: "https://discord.com/users/ali_mirdad#3700",
+  },
+  {
+    title: "Telegram",
+    icon: "/telegram.png",
+    link: "https://t.me/mir_alix",
+  },
+];
